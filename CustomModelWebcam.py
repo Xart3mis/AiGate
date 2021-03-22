b419133ddcb65457e6c31de9a5e03b5649a9723a
Xart3misX <xart3misx@protonmail.com>
2021-03-22 02:44:35 +0200

    Made it a bit faster by running on 0.25 res

--- CustomModelWebcam.py
+++ CustomModelWebcam.py
@@ -10,31 +10,36 @@ clf = joblib.load('model.pkl')
 
 while True:
     ret, frame = cap.read()
-
-    face_locations = face_recognition.face_locations(frame)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    face_locations = face_recognition.face_locations(small_frame)
     top = 0
     right = 0
     bottom = 0
-    name="unknown"
+    name = "unknown"
     left = 0
     try:
         for i in range(len(face_locations)):
-            frame_enc = face_recognition.face_encodings(frame)[i]
+            frame_enc = face_recognition.face_encodings(small_frame)[i]
             name = clf.predict([frame_enc])
             print(name)
-        
+
         top, right, bottom, left = face_locations[0]
     except IndexError:
         print("no faces")
-        
-    cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 1)
 
-    cv2.rectangle(frame, (left, top-30), (right, top), (0,255,0), -1)
-    cv2.putText(frame, name[0], (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+    top *= 4
+    right *= 4
+    bottom *= 4
+    left *= 4
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
+
+    cv2.rectangle(frame, (left, top-30), (right, top), (0, 255, 0), -1)
+    cv2.putText(frame, name[0], (left, top),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.namedWindow("face", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("face", 1280, 1280)
+    cv2.resizeWindow("face", 1280, 720)
     cv2.imshow('face', frame)
 
-    if cv2.waitKey(1) &0XFF == ord("q"):
+    if cv2.waitKey(1) & 0XFF == ord("q"):
         cv2.destroyAllWindows()
         cap.release()
