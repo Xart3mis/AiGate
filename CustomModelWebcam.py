9c3938ccd0e2bc86f8d730ff40ecf0181ab235f8
Xart3misX <xart3misx@protonmail.com>
2021-03-22 12:43:39 +0200

    runs facerec on 20% resolution instead of 25%   in an attempt to speed it up

--- CustomModelWebcam.py
+++ CustomModelWebcam.py
@@ -10,7 +10,7 @@ clf = joblib.load('model.pkl')
 
 while True:
     ret, frame = cap.read()
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
     face_locations = face_recognition.face_locations(small_frame)
     top = 0
     right = 0
@@ -27,17 +27,17 @@ while True:
     except IndexError:
         print("no faces")
 
-    top *= 4
-    right *= 4
-    bottom *= 4
-    left *= 4
+    top *= 5
+    right *= 5
+    bottom *= 5
+    left *= 5
     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
 
     cv2.rectangle(frame, (left, top-30), (right, top), (0, 255, 0), -1)
     cv2.putText(frame, name[0], (left, top),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
     cv2.namedWindow("face", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("face", 1280, 720)
+
     cv2.imshow('face', frame)
 
     if cv2.waitKey(1) & 0XFF == ord("q"):
