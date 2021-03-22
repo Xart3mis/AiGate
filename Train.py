e097cb3aca29bd1b08198454a45aa5fc5c4d3282
Xart3misX <xart3misx@protonmail.com>
2021-03-22 02:44:52 +0200

    fix path thing

--- Train.py
+++ Train.py
@@ -5,8 +5,7 @@ import face_recognition
 from sklearn.svm import SVC
 
 TRAIN_DIR = 'dataset/train/'
-persons = os.listdir(TRAIN_DIR)
-persons
+persons = os.listdir(os.getcwd() + TRAIN_DIR)
 
 #cv2.imshow('face', cv2.imread('dataset/train/face_1/adhast.1.jpg'))
 
@@ -18,12 +17,13 @@ names = []
 
 
 for person in persons:
-    
+
     pix = os.listdir(TRAIN_DIR + person)
 
     for person_img in pix:
 
-        face = face_recognition.load_image_file(TRAIN_DIR + person + "/" + person_img)
+        face = face_recognition.load_image_file(
+            TRAIN_DIR + person + "/" + person_img)
         face_bounding_boxes = face_recognition.face_locations(face)
 
         if len(face_bounding_boxes) == 1:
@@ -31,11 +31,11 @@ for person in persons:
             encodings.append(face_enc)
             names.append(person)
         else:
-            print(person + "/" + person_img + " was skipped and can't be used for training")
+            print(person + "/" + person_img +
+                  " was skipped and can't be used for training")
 
 clf = SVC()
 
 clf.fit(encodings, names)
 
 joblib.dump(clf, 'model.pkl')
-
