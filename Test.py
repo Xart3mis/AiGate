568244ef73b56ce8281ac054083a234f6b2512a1
Xart3misX <xart3misx@protonmail.com>
2021-04-06 10:20:36 +0200

    testing landmark prediction instead of face identification

--- Test.py
+++ Test.py
@@ -1,34 +1,30 @@
-import os
 import cv2
-import joblib
-import face_recognition
-
-clf = joblib.load('model.pkl')
-test_image = face_recognition.load_image_file('dataset/test/YoussefTest.jpg')
-
-face_recognition.face_encodings(test_image)
-
-face_locations = face_recognition.face_locations(test_image)
-
-print(f"Number of faces detected: {len(face_locations)}")
-
-for i in range(len(face_locations)):
-    test_image_enc = face_recognition.face_encodings(test_image)[i]
-    name = clf.predict([test_image_enc])
-    print(name)
-
-top, right, bottom, left = face_locations[0]
-
-test_image = cv2.imread('dataset/test/YoussefTest.jpg')
-
-cv2.rectangle(test_image, (left, top), (right, bottom), (0,255,0), 1)
-
-cv2.rectangle(test_image, (left, top-30), (right, top), (0,255,0), -1)
-cv2.putText(test_image, name[0], (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
-cv2.namedWindow("face", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("face", 1280, 720)
-cv2.imshow('face', test_image)
-
-cv2.waitKey()
+import dlib
+# Load the detector
+detector = dlib.get_frontal_face_detector()
+# Load the predictor
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+# read the image
+img = cv2.imread("dataset/train/Unknown/00018.png")
+# Convert image into grayscale
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+# Use detector to find landmarks
+faces = detector(gray)
+for face in faces:
+    x1 = face.left()  # left point
+    y1 = face.top()  # top point
+    x2 = face.right()  # right point
+    y2 = face.bottom()  # bottom point
+    # Look for the landmarks
+    landmarks = predictor(image=gray, box=face)
+    x = landmarks.part(27).x
+    y = landmarks.part(27).y
+    # Draw a circle
+    cv2.circle(img=img, center=(x, y), radius=5,
+               color=(0, 255, 0), thickness=-1)
+# show the image
+cv2.imshow(winname="Face", mat=img)
+# Wait for a key press to exit
+cv2.waitKey(delay=0)
+# Close all windows
 cv2.destroyAllWindows()
-
