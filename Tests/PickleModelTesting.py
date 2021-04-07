39978c1472b34b9a75036eac677d02cf50b941d4
Xart3misX <xart3misx@protonmail.com>
2021-04-07 16:52:44 +0200

    change encoding path and formatted code

--- Tests/PickleModelTesting.py
+++ Tests/PickleModelTesting.py
@@ -6,7 +6,7 @@ import dlib
 import time
 import cv2
 
-encodingsPath = "/home/bighero/AiGate/Models/yermam.pickle"
+encodingsPath = "D:\Documents\GitHub\AiGate\Models\goodboi.pickle"
 
 print("[INFO] loading encodings...")
 data = pickle.loads(open(encodingsPath, "rb").read())
@@ -21,68 +21,67 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
 while True:
-	frame = vs.read()
-	
-	currentTime = time.time()
-	fps = 1//(currentTime-previousTime)
-	previousTime = currentTime
-    
-	cv2.putText(frame, f"fps: {fps}", (30, 40),
-		    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 50), 2)
-	
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	rgb = imutils.resize(frame, width=526)
-	r = frame.shape[1] / float(rgb.shape[1])
-	
-	dets = detector(rgb)
-	boxes = []
-	for det in dets:
-		awooga = (det.top(), det.right(), det.bottom(), det.left())
-		boxes.append(awooga)
-	print(boxes)
-				
-	
-	if process:
-		encodings = face_recognition.face_encodings(rgb, boxes)
-		names = []
+    frame = vs.read()
 
-		for encoding in encodings:
+    currentTime = time.time()
+    fps = 1//(currentTime-previousTime)
+    previousTime = currentTime
 
-			matches = face_recognition.compare_faces(data["encodings"],
-				encoding)
-			name = "Unknown"
+    cv2.putText(frame, f"fps: {fps}", (30, 40),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 50), 2)
 
-			if True in matches:
-				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-				counts = {}
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb = imutils.resize(frame, width=526)
+    gray = cv2.cvtColor(src=rgb, code=cv2.COLOR_BGR2GRAY)
+    r = frame.shape[1] / float(rgb.shape[1])
 
-				for i in matchedIdxs:
-					name = data["names"][i]
-					counts[name] = counts.get(name, 0) + 1
-				name = max(counts, key=counts.get)
+    dets = detector(gray)
+    boxes = []
+    for det in dets:
+        awooga = (det.top(), det.right(), det.bottom(), det.left())
+        boxes.append(awooga)
+    print(boxes)
 
-			names.append(name)
-	process = not process
-	
-	for ((top, right, bottom, left), name) in zip(boxes, names):
-		top = int(top * r)
-		right = int(right * r)
-		bottom = int(bottom * r)
-		left = int(left * r)
+    if process:
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
 
-		cv2.rectangle(frame, (left, top), (right, bottom),
-			(0, 255, 0), 2)
-		y = top - 15 if top - 15 > 15 else top + 15
-		
-		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-			0.75, (0, 255, 0), 2)
-	
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+        for encoding in encodings:
 
-	if key == ord("q"):
-		break
+            matches = face_recognition.compare_faces(data["encodings"],
+                                                     encoding)
+            name = "Unknown"
+
+            if True in matches:
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
+
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
+                name = max(counts, key=counts.get)
+
+            names.append(name)
+    process = not process
+
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        top = int(top * r)
+        right = int(right * r)
+        bottom = int(bottom * r)
+        left = int(left * r)
+
+        cv2.rectangle(frame, (left, top), (right, bottom),
+                      (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+
+        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.75, (0, 255, 0), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
 
 cv2.destroyAllWindows()
 vs.stop()
-
