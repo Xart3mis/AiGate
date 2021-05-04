d394fc966722fafa8609217d98cda952dd49ec39
Xart3misX <xart3misx@protonmail.com>
2021-05-04 21:59:17 +0200

    douva do be retarded

--- Tests/deepFaceTest.py
+++ Tests/deepFaceTest.py
@@ -1,3 +1,11 @@
 from deepface import DeepFace
-result  = DeepFace.verify("img1.jpg", "img2.jpg")
-print("Is verified: ", result["verified"])
+
+models = ["VGG-Face", "Facenet", "OpenFace",
+          "DeepFace", "DeepID", "ArcFace", "Dlib"]
+
+for model in models:
+    result = DeepFace.verify(
+        "D:\\Documents\\dataset\\train\seif zayed\\20210401_002105.jpg",
+        "D:\\Documents\\dataset\\train\\seif zayed\\20210401_002113.jpg", model_name=model)
+
+    print(result["verified"])
