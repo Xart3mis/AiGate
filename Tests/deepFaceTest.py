f9976f85b5e81c58ddb8586d357149a0ea684816
Xart3misX <xart3misx@protonmail.com>
2021-05-09 11:13:03 +0200

    shit real-time now ig

--- Tests/deepFaceTest.py
+++ Tests/deepFaceTest.py
@@ -1,11 +1,44 @@
 from deepface import DeepFace
+from imutils.video import VideoStream
+import cv2
+import time
 
-models = ["VGG-Face", "Facenet", "OpenFace",
-          "DeepFace", "DeepID", "ArcFace", "Dlib"]
+imag1 = "D:\\Documents\\dataset\\train\seif zayed\\20210401_002105.jpg"
+imag2 = "D:\\Documents\\dataset\\train\\seif zayed\\20210401_002113.jpg"
 
-for model in models:
-    result = DeepFace.verify(
-        "D:\\Documents\\dataset\\train\seif zayed\\20210401_002105.jpg",
-        "D:\\Documents\\dataset\\train\\seif zayed\\20210401_002113.jpg", model_name=model)
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
 
-    print(result["verified"])
+previousTime = 0
+
+while True:
+    frame = vs.read()
+
+    currentTime = time.time()
+    fps = 1//(currentTime-previousTime)
+    previousTime = currentTime
+
+    cv2.putText(frame, f"fps: {fps}", (30, 40),
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 50), 2)
+
+    tic = time.time()
+    try:
+        detected_face = DeepFace.detectFace(
+            frame, detector_backend="ssd")
+    except ValueError:
+        detected_face = cv2.cvtColor(detected_face, cv2.COLOR_RGB2BGR)
+        print("nu face")
+
+    toc = time.time()
+
+    print(toc-tic)
+
+    detected_face = cv2.cvtColor(detected_face, cv2.COLOR_RGB2BGR)
+    cv2.imshow('siodoai', detected_face)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+exit(0)
